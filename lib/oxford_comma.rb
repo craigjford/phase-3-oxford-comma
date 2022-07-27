c26f72883d7582f9e78cb15fd2e595a35d2fa829
Craig Ford <craig_ford@icloud.com>
2022-07-27 12:00:06 -0400

    commit tree 16ea731c5bbcabc25dfd31a911af13dae9f3367e

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,17 @@
-def oxford_comma(array)
 
+
+def oxford_comma(array)
+  arr_str = ""  
+  case array.length
+    when 1
+      arr_str = array[0]
+    when 2
+      arr_str = array[0] + " and " + array[1]
+    else
+      array[array.length - 1] = "and " + array[array.length - 1]
+      arr_str = array.join(", ")
+    end            
 end
+
+
+
